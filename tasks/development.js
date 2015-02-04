84cf7425ea329644ad4af89cb424d6766a34afaa
hung-phan <colorvisavn@gmail.com>
2015-02-05 01:52:03 +0700

    Test config

--- tasks/development.js
+++ tasks/development.js
@@ -4,3 +4,15 @@
  * Development config
  */
 var config = require('./config.json');
+var gulp   = require('gulp');
+var del    = require('del');
+
+gulp.task('development:clean', function () {
+  // remove bundle.js file
+  del(['./app/assets/javascripts/bundle.js'], function (err, paths) {
+    console.log('Deleted files/folders:\n', paths.join('\n'));
+  });
+});
+
+//gulp.task('development:build', ['development:clean'], function() {
+//});
