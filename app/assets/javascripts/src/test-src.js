ba297cd67ba94e8c618858fbe95b19b892925fe7
hung-phan <colorvisavn@gmail.com>
2015-02-05 03:16:19 +0700

    Update task

--- app/assets/javascripts/src/test-src.js
+++ app/assets/javascripts/src/test-src.js
@@ -1,1 +1,1 @@
-Hello
+var $ = require('jquery');
--- tasks/development.js
+++ tasks/development.js
@@ -1,32 +1,53 @@
 /*
  * @author Hung Quang Phan
- * 
+ *
  * Development config
  */
 
-var config     = require('./config.json');
-var del        = require('del');
-var gulp       = require('gulp');
-var gutil      = require('gulp-util');
-var plumber    = require('gulp-plumber');
+var config       = require('./config.json');
+var del          = require('del');
+var gulp         = require('gulp');
+var gutil        = require('gulp-util');
+var plumber      = require('gulp-plumber');
 
-var sourcemaps = require('gulp-sourcemaps');
-var es6ify     = require('es6ify');
-var browserify = require('browserify');
-var source     = require('vinyl-source-stream');
-var buffer     = require('vinyl-buffer');
+var sourcemaps   = require('gulp-sourcemaps');
+var es6ify       = require('es6ify');
+var debowerify   = require('debowerify');
+var browserify   = require('browserify');
+var source       = require('vinyl-source-stream');
+var buffer       = require('vinyl-buffer');
+var watchify     = require('watchify');
+var literalify   = require('literalify').configure(config.browserify.transform.literalify);
+var Notification = require('node-notifier');
 
+var errorHandlers = {
+  browserifyErrorHandler: function (err) {
+     (new Notification()).notify({ message: 'Error: ' + err.message });
+     gutil.log(util.colors.red('Error'), err.message);
+  }
+};
+
+// main task
 gulp.task('development:clean', function () {
   del([config.development.build], function (err, paths) {
-    gutil.log('Deleted files/folders:\n', gutil.colors.cyan(paths.join('\n')));
+    gutil.log(
+      'Deleted files/folders:\n',
+      gutil.colors.cyan(paths.join('\n'))
+    );
   });
 });
 
 gulp.task('development:build', function() {
-  var stream = gulp.src(config.development.src)
-                 .pipe(plumber())
-                 .pipe(plumber.stop())
-                 .pipe(gulp.dest(config.development.build))
+  var bundler = browserify(config.browserify.settings);
+
+  bundler.add('./app/assets/javascripts/src/test-src.js');
+  bundler.transform(literalify);
+  bundler.transform(debowerify);
+
+  var stream = bundler.bundle()
+                 .on('error', errorHandlers.browserifyErrorHandler)
+                 .pipe(source('test-src.js'))
+                 .pipe(gulp.dest(config.development.build));
 
   return stream;
 });
