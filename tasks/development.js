da58c007e07d56510bf555419734b63b0ee7d837
hung-phan <colorvisavn@gmail.com>
2015-02-05 02:14:50 +0700

    Update color on delete files

--- tasks/development.js
+++ tasks/development.js
@@ -6,13 +6,14 @@
 var config = require('./config.json');
 var gulp   = require('gulp');
 var del    = require('del');
+var gutil  = require('gulp-util');
 
 gulp.task('development:clean', function () {
-  // remove bundle.js file
-  del([config.development.dest], function (err, paths) {
-    console.log('Deleted files/folders:\n', paths.join('\n'));
+  del([config.development.build], function (err, paths) {
+    gutil.log('Deleted files/folders:\n', gutil.colors.cyan(paths.join('\n')));
   });
 });
 
-//gulp.task('development:build', ['development:clean'], function() {
-//});
+gulp.task('development:build', ['development:clean'], function() {
+
+});
