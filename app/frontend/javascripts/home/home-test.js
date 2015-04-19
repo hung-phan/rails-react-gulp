d3db58e79b01c653254a43360aabd5421f0780ff
hung-phan <colorvisavn@gmail.com>
2015-04-19 21:46:24 +0700

    Using nvmrc

--- app/frontend/javascripts/home/home-test.js
+++ app/frontend/javascripts/home/home-test.js
@@ -1,8 +1,21 @@
-let assert = require("assert");
+'use strict';
 
-describe('dummy test', () => {
-  it('should show something', () => {
-    assert.equal(-1, [1,2,3].indexOf(5));
-    assert.equal(-1, [1,2,3].indexOf(0));
+import React from 'react/addons';
+import assert from 'assert';
+import { jsdom } from 'jsdom';
+
+if (typeof document === 'undefined') {
+  global.document = jsdom('<html><body></body></html>');
+  global.window = document.defaultView;
+  global.navigator = {userAgent: 'node.js'};
+}
+
+describe('Home Component', () => {
+  it('should bind to DOM node', () => {
+    let TestUtils = React.addons.TestUtils;
+    let Home = require('./home');
+
+    let Component = TestUtils.renderIntoDocument(React.createElement(Home));
+    TestUtils.findRenderedDOMComponentWithClass(Component, 'container');
   });
 });
